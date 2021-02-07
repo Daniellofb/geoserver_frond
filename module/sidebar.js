13c481efe66f9368b4ec33cdbe67d28726800acf
DanielFKB <daniel@isinet.ws>
2021-02-06 19:55:02 -0600

    Fix - Cambio en estilos

--- module/sidebar.js
+++ module/sidebar.js
@@ -6,98 +6,7 @@
             // Primer apartado, cargado de mapas bases.
             new ol.layer.Group({
                 title: 'Mapa Base(Temas)',
-                layers: [
-                    new ol.layer.Group({
-                        title: 'Terrain Colors Con Etiquetas',
-                        type: 'base',
-                        combine: true,
-                        visible: false,
-                        layers: [
-                            new ol.layer.Tile({
-                                source: new ol.source.Stamen({
-                                    layer: 'terrain'
-                                })
-                            }),
-                            new ol.layer.Tile({
-                                source: new ol.source.Stamen({
-                                    layer: 'terrain-labels'
-                                })
-                            })
-                        ]
-                    }),
-                    new ol.layer.Tile({
-                        title: 'Terreno Colors',
-                        type: 'base',
-                        visible: false,
-                        source: new ol.source.Stamen({
-                            layer: 'terrain'
-                        })
-                    }),
-                    new ol.layer.Group({
-                        title: 'Toner Colors Con Etiquetas',
-                        type: 'base',
-                        combine: true,
-                        visible: false,
-                        layers: [
-                            new ol.layer.Tile({
-                                source: new ol.source.Stamen({
-                                    layer: 'toner'
-                                })
-                            }),
-                            new ol.layer.Tile({
-                                source: new ol.source.Stamen({
-                                    layer: 'terrain-labels'
-                                })
-                            })
-                        ]
-                    }),
-                    new ol.layer.Tile({
-                        title: 'Toner Colors',
-                        type: 'base',
-                        visible: false,
-                        source: new ol.source.Stamen({
-                            layer: 'toner'
-                        })
-                    }),
-                    new ol.layer.Group({
-                        title: 'Aqua Colors Con Etiquetas',
-                        type: 'base',
-                        combine: true,
-                        visible: false,
-                        layers: [
-                            new ol.layer.Tile({
-                                source: new ol.source.Stamen({
-                                    layer: 'watercolor'
-                                })
-                            }),
-                            new ol.layer.Tile({
-                                source: new ol.source.Stamen({
-                                    layer: 'terrain-labels'
-                                })
-                            })
-                        ]
-                    }),
-                    new ol.layer.Tile({
-                        title: 'Aqua Colors',
-                        type: 'base',
-                        visible: false,
-                        source: new ol.source.Stamen({
-                            layer: 'watercolor'
-                        })
-                    }),
-                    new ol.layer.Tile({
-                        title: 'OSM',
-                        type: 'base',
-                        visible: true,
-                        source: new ol.source.OSM()
-                    }),
-                    /*new ol.layer.Tile({
-                      title: 'Satélite',
-                      type: 'base',
-                      visible: false,
-                      source: new ol.source.MapQuest({layer: 'sat'})
-                    }),*/
-                ]
+                layers: cargaStamen()
             }),
 
             // Segundo apartado, cargado de capas.
@@ -130,130 +39,130 @@
 // Carga de capas de IMMX
 function cargaImmx() {
     var data = [
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Colonias',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'colonias_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Area Geoestadistica Municipal',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'areageoestadisticabasicamunicipal_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Area Geoestadistica',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'areageoestadisticabasicarural_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Localidades Rurales',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'localidadesurbanasrurales_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Localidades Urbanas',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'localidadurbanasrurales_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Zona Metropolitana',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'zonametropoliana_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Cuerpos de Agua',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'cuerposagua_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Áreas Naturales Protegidas',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'anp_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'AGED Rural(AM)',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'agebruralam_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'AGED Rural',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'agebrural_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Codigo Postal',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'cp_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Codigo Postal(AM)',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'cpam_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Municipio',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: {
                     LAYERS: 'municipio_poligonos',
@@ -263,65 +172,156 @@ function cargaImmx() {
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Limites Municipales',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'limitemunicipio_line' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Ríos',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'rios_linestring' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Montañas',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'curvas_poligonos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Localidades Rurales Limites(Puntos)',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'localidadesruralesglobales_points' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Localidades Rurales(Puntos)',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'localidadesrurales_points' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
         }),
-        new ol.layer.Image({
+        new ol.layer.Tile({
             title: 'Centro de Gestión Comunitaria(CGC)',
             visible: false,
-            source: new ol.source.ImageWMS({
+            source: new ol.source.TileWMS({
                 ratio: 1,
                 params: { LAYERS: 'cgc_puntos' },
                 url: 'http://cartografia.xalapa.gob.mx/geoserver/wms',
                 serverType: 'geoserver'
             })
+        }),
+    ];
+
+    return data;
+}
+
+function cargaStamen() {
+    var data = [
+        new ol.layer.Group({
+            title: 'Terrain Colors Con Etiquetas',
+            type: 'base',
+            combine: true,
+            visible: false,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'terrain'
+                    })
+                }),
+                new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'terrain-labels'
+                    })
+                })
+            ]
+        }),
+        new ol.layer.Tile({
+            title: 'Terreno Colors',
+            type: 'base',
+            visible: false,
+            source: new ol.source.Stamen({
+                layer: 'terrain'
+            })
+        }),
+        new ol.layer.Group({
+            title: 'Toner Colors Con Etiquetas',
+            type: 'base',
+            combine: true,
+            visible: false,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'toner'
+                    })
+                }),
+                new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'terrain-labels'
+                    })
+                })
+            ]
+        }),
+        new ol.layer.Tile({
+            title: 'Toner Colors',
+            type: 'base',
+            visible: false,
+            source: new ol.source.Stamen({
+                layer: 'toner'
+            })
+        }),
+        new ol.layer.Group({
+            title: 'Aqua Colors Con Etiquetas',
+            type: 'base',
+            combine: true,
+            visible: false,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'watercolor'
+                    })
+                }),
+                new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'terrain-labels'
+                    })
+                })
+            ]
+        }),
+        new ol.layer.Tile({
+            title: 'Aqua Colors',
+            type: 'base',
+            visible: false,
+            source: new ol.source.Stamen({
+                layer: 'watercolor'
+            })
+        }),
+        new ol.layer.Tile({
+            title: 'OSM',
+            type: 'base',
+            visible: true,
+            source: new ol.source.OSM()
         }),
     ];
 
